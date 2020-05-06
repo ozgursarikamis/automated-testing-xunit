d7f8bdd79b29971d65ea1815882a541db33587d5
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-05-06 03:35:27 +0300

    first commit

---
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CreditCards.UITests
+{
+    public class CreditCardWebAppShould
+    {
+        [Fact, Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            // smoke test
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:44108/");
+            }
+        }
+    }
+}
