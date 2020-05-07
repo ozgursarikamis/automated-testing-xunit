0a4e54a88a3435fcb1ea69219566ba54f21e90c8
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-05-07 03:54:57 +0300

    CSS Selectors: ClassName :star:

--- CreditCards.UITests/CreditCardApplicationShould.cs
+++ CreditCards.UITests/CreditCardApplicationShould.cs
@@ -5,7 +5,7 @@ using Xunit;
 namespace CreditCards.UITests
 {
     public class CreditCardApplicationShould
-    { 
+    {
         private const string HomeUrl = "http://localhost:44108/";
         private const string ApplyUrl = "http://localhost:44108/Apply";
 
@@ -45,6 +45,30 @@ namespace CreditCards.UITests
                 Assert.Equal("Credit Card Application - Credit Cards", driver.Title);
                 Assert.Equal(ApplyUrl, driver.Url);
             }
-        } 
+        }
+
+        [Fact]
+        public void BeInitiatedFromHomePage_CustomerService()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+                DemoHelper.Pause();
+
+                IWebElement carouselNext = driver.FindElement(By.CssSelector("[data-slide='next']"));
+                carouselNext.Click();
+                DemoHelper.Pause(1000); // allow carousel to scroll
+                carouselNext.Click();
+                DemoHelper.Pause(1000); // allow carousel to scroll
+
+                IWebElement applyLink = driver.FindElement(By.ClassName("customer-service-apply-now"));
+                applyLink.Click();
+
+                DemoHelper.Pause();
+
+                Assert.Equal("Credit Card Application - Credit Cards", driver.Title);
+                Assert.Equal(ApplyUrl, driver.Url);
+            }
+        }
     }
 }
