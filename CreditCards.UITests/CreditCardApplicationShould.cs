3af57b39f7bfdc5f1082204d8e92c41a65bf4dcd
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-05-07 03:31:12 +0300

    interaction elements using IWebElement: apply lnk clicked

---
+++ CreditCards.UITests/CreditCardApplicationShould.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace CreditCards.UITests
+{
+    public class CreditCardApplicationShould
+    { 
+        private const string HomeUrl = "http://localhost:44108/";
+        private const string ApplyUrl = "http://localhost:44108/Apply";
+
+        [Fact]
+        public void BeInitiatedFromHomePage_NewLowRate()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+                DemoHelper.Pause();
+
+                IWebElement applyLink = driver.FindElement(By.Name("ApplyLowRate"));
+                applyLink.Click();
+
+                DemoHelper.Pause();
+                Assert.Equal("Credit Card Application - Credit Cards", driver.Title);
+                Assert.Equal(ApplyUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        public void BeInitiatedFromHomePage_EasyApplication()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+                DemoHelper.Pause(11000);
+
+                IWebElement applyLink = driver.FindElement(By.LinkText("Easy: Apply Now!"));
+                applyLink.Click();
+                DemoHelper.Pause();
+
+                Assert.Equal("Credit Card Application - Credit Cards", driver.Title);
+                Assert.Equal(ApplyUrl, driver.Url);
+            }
+        }
+    }
+}
