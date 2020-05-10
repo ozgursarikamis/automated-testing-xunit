b135b45cea8b75dc4c08434f3c5c98b57613408b
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-05-10 04:17:37 +0300

    javascript

---
+++ CreditCards.UITests/JavaScriptExamples.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace CreditCards.UITests
+{
+    public class JavaScriptExamples
+    {
+        [Fact]
+        public void ClickOverlayeredLink()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:44108/JSOverlay.html");
+                
+                const string script = "return document.getElementById('HiddenLink').innerHTML;";
+                IJavaScriptExecutor js = (IJavaScriptExecutor) driver;
+             
+                var linkText = (string) js.ExecuteScript(script);
+                Assert.Equal("Go to Pluralsight", linkText);
+            }
+        }
+    }
+}
